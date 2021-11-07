989472369e18493cd8d929e628b78f7cbad7e2f7
HOME-PC\Home <sadishkr@gmail.com>
2021-11-07 12:18:43 +1100

    Rebase

---
+++ arithmetics.py
@@ -0,0 +1,11 @@
+#Testing operators and arithmetics
+
+
+a = "1abc1abc1abc1bac"
+print(a.replace('abc','ABC'))
+print("*************")
+
+sub = a[1:4]
+step = a[0:16:4]
+print(sub)
+print(step)
---
+++ variables.py
@@ -0,0 +1,9 @@
+"""
+Table of variables
+"""
+
+import keyword
+print(keyword.kwlist)
+
+variable_a = 10
+
